d749428995240f1322c3131ad7cfb229635408ef
Komediruzecki <komediruzecki.2015@gmail.com>
2024-10-23 00:49:26 +0200

    Add initial login component google sign in

--- tabu-app/src/components/LoginItem.vue
+++ tabu-app/src/components/LoginItem.vue
@@ -1,8 +1,33 @@
 <script setup lang="ts">
 import IconTabu from '@/components/icons/IconTabu.vue'
+import { ref } from 'vue'
+
+let googleClientId = ref(
+  '881939165116-d8tp8es5j5v6trie65vh5feaa8op9erg.apps.googleusercontent.com',
+)
 </script>
 
 <template>
   <div>Login to Tabu</div>
+  <div
+    id="g_id_onload"
+    data-client_id="{{googleClientId}}"
+    data-context="signin"
+    data-ux_mode="popup"
+    data-callback="googleLoginCallback"
+    data-auto_prompt="false"
+  ></div>
+
+  <div
+    class="g_id_signin"
+    data-type="standard"
+    data-shape="pill"
+    data-theme="outline"
+    data-text="continue_with"
+    data-size="large"
+    data-locale="en-GB"
+    data-logo_alignment="center"
+    data-width="400"
+  ></div>
   <icon-tabu />
 </template>
